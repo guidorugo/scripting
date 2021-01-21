7963431331381d783950f5e854db2ae55798dc35
Guido <guido.rugo@gmail.com>
2021-01-21 20:27:39 +0000

    Add marathon-scale-all.py
    Script to scale down or up all apps in Marathon.
    
    Scales down to 0 or up to 1.

---
+++ marathon-scale-all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import requests
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', '-u', metavar='<stooge>', nargs='?', help='Marathon URL')
+parser.add_argument('--up', action='store_true', help='Scale all up to 1')
+parser.add_argument('--down', action='store_true', help='Scale all down to 0')
+args = parser.parse_args()
+
+if (args.up and args.down) or (not args.up and not args.down):
+    sys.exit('You should specify one and only one action.')
+
+if not args.url:
+    parser.print_help(sys.stderr)
+    sys.exit('\nMissing Marathon URL')
+
+marathon = str(args.url)+'/v2/apps'
+get_apps = requests.get(marathon)
+if args.up:
+    payload = {'instances': 1}
+    for each_app in get_apps.json()['apps']:
+        requests.put(marathon+each_app['id']+"?force=true", json=payload)
+        print(each_app+' set to 1')
+
+elif args.down:
+    payload = {'instances': 0}
+    for each_app in get_apps.json()['apps']:
+        requests.put(marathon+each_app['id']+"?force=true", json=payload)
+        print(each_app+' set to 0')
